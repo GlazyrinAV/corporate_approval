83d65c05159d05068d83844db541f8c1f064faee
Алексей <118848145+GlazyrinAV@users.noreply.github.com>
2026-01-19 21:36:35 +0300

    feat: новый модуль

---
+++ server_users/src/main/java/ru/avg/server_users/ServerUsersApplication.java
@@ -0,0 +1,13 @@
+package ru.avg.server_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerUsersApplication.class, args);
+    }
+
+}
---
+++ server_users/src/test/java/ru/avg/server_users/ServerUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avg.server_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
